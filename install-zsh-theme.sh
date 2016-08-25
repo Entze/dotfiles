81a0ef068c23d729935599c7f736818e9a55832d
Lukas Grassauer <entze@grassauer.eu>
2016-08-25 14:23:22 +0200

    refined patch

--- install-zsh-theme.sh
+++ install-zsh-theme.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 cd ./zsh/.oh-my-zsh
 git submodule add -f https://github.com/bhilburn/powerlevel9k.git ./custom/themes/powerlevel9k
+printf "attempt to copy the patched upgrade file\n"
 cp -f ../../upgrade.sh ./tools/upgrade.sh
+printf "done."
