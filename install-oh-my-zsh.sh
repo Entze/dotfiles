06b8ed94f4b09e71d0ba6f62f9e388ca855fd27b
Lukas Grassauer <entze@grassauer.eu>
2016-09-22 13:17:44 +0200

    refinded notices

--- install-oh-my-zsh.sh
+++ install-oh-my-zsh.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-printf "attempt to add oh my zsh"
+printf "attempt to add oh my zsh\n"
 git submodule add -f https://github.com/robbyrussell/oh-my-zsh ./zsh/.oh-my-zsh
-printf "done."
+printf "done\n"
