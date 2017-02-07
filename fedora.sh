e775d44ada7e92fc824ded1104d6eae78c45690d
Lukas Grassauer <entze@grassauer.eu>
2017-02-07 18:16:18 +0100

    add util-linux-user & beautify output

--- fedora.sh
+++ fedora.sh
@@ -4,20 +4,20 @@ rm -f $HOME/.gitconfig
 git config --global user.name "Lukas Grassauer"
 git config --global user.email "entze@grassauer.eu"
 sudo dnf install -y etckeeper
-printf "done\n"
+printf "done\n-----\n"
 printf "update installed packages\n"
 sudo dnf update
-printf "done\n"
+printf "done\n-----\n"
 sudo dnf config-manager -y --add-repo http://download.opensuse.org/repositories/home:Horst3180/Fedora_24/home:Horst3180.repo
 printf "installing all packages in packages.list"
 sudo dnf install --best $(cat $HOME/dotfiles/packages.list)
-printf "done\n"
+printf "done\n-----\n"
 printf "changing the shell to zsh\n"
 chsh -s /usr/bin/zsh
-printf "done\n"
+printf "done\n-----\n"
 printf "restoring gitconfig"
 rm -f $HOME/.gitconfig
-printf "done\n"
+printf "done\n-----\n"
 printf "stowing dotfiles"
 cd ~/dotfiles/.
 stow --verbose=1 emacs
@@ -25,7 +25,7 @@ stow --verbose=1 git
 stow --verbose=1 vim
 stow --verbose=1 xfce
 stow --verbose=1 zsh
-printf "done\n"
+printf "done\n-----\n"
 $HOME/dotfiles/configure-ssh.sh
 $HOME/dotfiles/install-emacs-plugins.sh
 $HOME/dotfiles/install-solarized-theme.sh
@@ -33,6 +33,6 @@ $HOME/dotfiles/install-oh-my-zsh.sh
 $HOME/dotfiles/install-zsh-theme.sh
 printf "update icon cache\n"
 gtk-update-icon-cache ~/.icons/Numix-Circle
-printf "done\n"
+printf "done\n-----\n"
 ln -s ~/.config/xfce4/terminal/Darkterminalrc ~/.config/xfce4/terminal/terminalrc
 printf "done with everything.\nCheck and then restart.\n"
