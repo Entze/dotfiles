c82bed9f1ff034bd09fb698ef8807e5de6c9a952
Lukas Grassauer <entze@grassauer.eu>
2017-04-09 00:02:24 +0200

    new emacs config

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1,3 +1,4 @@
+;load the configuration.org
 
 ;; Added by Package.el.  This must come before configurations of
 ;; installed packages.  Don't delete this line.  If you don't want it,
@@ -11,9 +12,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (gradle-mode evil-paredit paredit relative-line-numbers flycheck-ledger ledger-mode flycheck-package auctex smex yasnippet ox-twbs graphviz-dot-mode ox-mediawiki golden-ratio web-mode flycheck-color-mode-line flycheck magit-gitflow magit typescript-mode coffee-mode haskell-mode sass-mode helm rainbow-delimiters rainbow-mode diff-hl htmlize json-mode evil-surround solarized-theme evil pallet))))
+ '(package-selected-packages (quote (haskell-mode evil solarized-theme pallet))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
