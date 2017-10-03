5311a59c9e8426372f845666988c802e2cb6d48e
Lukas Grassauer <lukas@grassauer.eu>
2017-10-03 22:44:07 +0200

    snap

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -12,7 +12,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (haskell-mode evil solarized-theme pallet))))
+ '(package-selected-packages
+   (quote
+    (sass-mode typescript-mode haskell-mode evil solarized-theme pallet))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
