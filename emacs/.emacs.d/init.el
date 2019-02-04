f24441c5ccae5fb487e2dd0d0889cda833f256fb
Lukas Grassauer <lukas@grassauer.eu>
2019-02-04 23:44:41 +0100

    Snapshot 2019-02-04T23:44:41+01:00

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1,23 +1,88 @@
-;load the configuration.org
+(use-package package)
+(require 'package)
+(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/") t)
+(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(add-to-list 'package-archives '("gnu" . "https://elpa.gnu.org/packages/") t)
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
 
-;; Added by Package.el.  This must come before configurations of
-;; installed packages.  Don't delete this line.  If you don't want it,
-;; just comment it out by adding a semicolon to the start of the line.
-;; You may delete these explanatory comments.
+;; Higher values are searched first.
+(setq package-archive-priorities
+      '(("org"          . 200)
+        ("melpa-stable" . 150)
+        ("melpa"        . 100)
+        ("marmalade"    . 75)
+        ("gnu"          . 50)))
+
 (package-initialize)
 
-(org-babel-load-file "~/.emacs.d/configuration.org")
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (hlint-refactor org-plus-contrib org langtool markdown-mode sass-mode typescript-mode haskell-mode evil solarized-theme pallet))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+(when (not package-archive-contents)
+  (package-refresh-contents))
+
+(defvar my-packages
+  '(auctex auto-complete evil haskell-mode idea-darkula-theme iso-transl org org-bullets ox-md sass-mode typescript-mode undo-tree use-package) 
+  "A list of packages to ensure are installed at launch.")
+
+(dolist (p my-packages)
+  (when (not (package-installed-p p))
+    (package-install p)))
+
+(load-file "~/.emacs.d/opt/sensible-defaults.el")
+(sensible-defaults/use-all-settings)
+(sensible-defaults/use-all-keybindings)
+(sensible-defaults/backup-to-temp-directory)
+
+(when window-system
+   (load-theme 'idea-darkula t))
+
+(setq org-src-fontify-natively t)
+
+(global-prettify-symbols-mode t)
+
+(when window-system
+   (scroll-bar-mode -1))
+
+(when window-system
+    (global-hl-line-mode))
+
+(evil-mode 1)
+
+(add-hook 'text-mode-hook 'turn-on-auto-fill)
+(add-hook 'gfm-mode-hook 'turn-on-auto-fill)
+(add-hook 'org-mode-hook 'turn-on-auto-fill)
+
+(add-hook 'haskell-mode-hook
+          (lambda ()
+            (haskell-doc-mode)
+            (turn-on-haskell-indent)
+            (ghc-init)))
+
+(eval-after-load "org"
+  '(require 'ox-md nil t))
+
+(add-to-list 'ispell-skip-region-alist '(":\\(PROPERTIES\\|LOGBOOK\\):" . ":END:"))
+(add-to-list 'ispell-skip-region-alist '("#\\+BEGIN_SRC" . "#\\+END_SRC"))
+
+(add-hook 'org-mode-hook (lambda ()
+   (progn
+     ;; Snipped
+     (auto-fill-mode t)
+
+     ;; Spelling
+     (flyspell-mode t))))
+
+(setq org-export-with-smart-quotes t)
+
+(setq org-latex-pdf-process
+'("pdflatex -interaction nonstopmode -output-directory %o %f"
+        "biber %b"
+        "pdflatex -interaction nonstopmode -output-directory %o %f"
+        "pdflatex -interaction nonstopmode -output-directory %o %f"))
+
+(setq-default indent-tabs-mode nil)
+
+(setq-default tab-width 2)
+
+(setq ring-bell-function 'ignore)
+
+(require 'iso-transl)
