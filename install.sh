b49f6146ccbbdc4c649bafbe07faa316b74c11dd
Lukas Grassauer <lukas@grassauer.eu>
2020-01-05 17:46:22 +0100

    add a sensible vimrc based on the default

--- install.sh
+++ install.sh
@@ -2,8 +2,11 @@
 
 STOWING=$(find . -mindepth 1 -maxdepth 1 -type d -not -wholename "./.*" -exec basename {} \; | tr '\n' ' ' | sed -E "s/[ \t]*$//")
 
+printf "mkdir -p $HOME/.vim/swap $HOME/.vim/undo $HOME/.vim/backup\n"
+mkdir -p $HOME/.vim/swap $HOME/.vim/undo $HOME/.vim/backup
+
+printf "stow --target=$HOME --delete --verbose=1 $STOWING"
 stow --target=$HOME --delete --verbose=1 $STOWING
+printf "stow --target=$HOME --verbose=1 $STOWING"
 stow --target=$HOME --verbose=1 $STOWING
-
-
 
