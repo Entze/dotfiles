5c648d0af3bad4ae1af00b6b6b6822c2af649a45
Lukas Grassauer <lukas@grassauer.eu>
2020-03-03 10:30:19 +0100

    turn on autofill for programming

--- emacs/.emacs.d/programming.el
+++ emacs/.emacs.d/programming.el
@@ -1,3 +1,7 @@
 
-(load "~/.emacs.d/haskell.el")
+(setq-default fill-column 80)
 
+(add-hook 'text-mode-hook 'turn-on-auto-fill)
+(global-set-key (kbd "C-c q") 'auto-fill-mode)
+
+(load "~/.emacs.d/haskell.el")
