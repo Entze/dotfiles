c642995969da5bf15e853a32c7ecf1f6dfc494ef
Lukas Grassauer <lukas@grassauer.eu>
2020-03-07 12:14:46 +0100

    add Uni to Agenda

--- emacs/.emacs.d/orgconf.el
+++ emacs/.emacs.d/orgconf.el
@@ -1,9 +1,26 @@
 
 
+(defun org-get-agenda-files-recursively (dir)
+  "Get org agenda files from root DIR."
+  (directory-files-recursively dir "\.org$"))
+
+(defun org-set-agenda-files-recursively (dir)
+ "Set org-agenda files from root DIR."
+ (setq org-agenda-files
+   (org-get-agenda-files-recursively dir)))
+
+(defun org-add-agenda-files-recursively (dir)
+  "Add org-agenda files from root DIR."
+  (nconc org-agenda-files
+   (org-get-agenda-files-recursively dir)))
+
+
 (setq org-directory (expand-file-name "~/Org"))
 (setq org-default-notes-file (concat org-directory "/main.org"))
+(setq org-archive-location "~/Org/archive.org::* From %s")
 (setq org-agenda-files '("~/Org"))
 
+(org-add-agenda-files-recursively "~/Uni")
 
 (setq org-todo-keywords
       '(
