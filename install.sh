80d0f705fb9cfc6a8e1d042d1ae528af132f4175
Lukas Grassauer <lukas@grassauer.eu>
2023-01-16 12:40:14 +0100

    fix: remove stow of emacs

--- install.sh
+++ install.sh
@@ -8,11 +8,9 @@ mkdir -p "$HOME"/.vim/swap "$HOME"/.vim/undo "$HOME"/.vim/backup
 stow --target="$HOME" --delete --verbose=1 git
 stow --target="$HOME" --delete --verbose=1 vim
 stow --target="$HOME" --delete --verbose=1 zsh
-stow --target="$HOME" --delete --verbose=1 emacs
 stow --target="$HOME" --delete --verbose=1 python
 
 stow --target="$HOME" --verbose=1 git
 stow --target="$HOME" --verbose=1 vim
 stow --target="$HOME" --verbose=1 zsh
-stow --target="$HOME" --verbose=1 emacs
 stow --target="$HOME" --verbose=1 python
