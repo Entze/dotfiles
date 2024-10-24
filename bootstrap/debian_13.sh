a2e69fdaf5878f7cd28892355a72f2f334061442
Lukas Grassauer <lukas@grassauer.eu>
2024-10-24 17:20:55 +0200

    Fix detection whether a pkg is already installed
    Furthermore the code was reorganized to be more modular.

--- bootstrap/debian_13.sh
+++ bootstrap/debian_13.sh
@@ -2,37 +2,54 @@
 
 function ensure_installed() {
 
+  pkgs="$(mktemp 'bootstrap-debian-13.XXXX')"
+  dpkg --list > "$pkgs"
+
   for pkg in "$@"; do
-    listed="$(dpkg --list | grep --quiet "ii[[:space:]]+${pkg}")"
-    if [[ "$listed" -eq 1 ]]; then
+    grep --quiet "ii[[:space:]]+${pkg}" "$pkgs"
+    if [[ "$?" -eq 1 ]]; then
       sudo apt-get install --assume-yes "$pkg"
     fi
   done
 
 }
 
-listed="$(dpkg --list | grep --quiet "ii[[:space:]]+mise")"
-if [[ "$listed" -eq 1 ]]; then
 
-  sudo apt-get update --assume-yes
-  sudo apt-get upgrade --assume-yes
-  ensure_installed gpg sudo wget curl
-  sudo install -dm 755 /etc/apt/keyrings
-  wget -qO - "https://mise.jdx.dev/gpg-key.pub" | gpg --dearmor | sudo tee /etc/apt/keyrings/mise-archive-keyring.gpg
+function install_mise() {
 
-  printf "X-Repolib-Name: mise
-Enabled: yes
-Types: deb
-URIs: https://mise.jdx.dev/deb
-Suites: stable
-Components: main
-Signed-By: /etc/apt/keyrings/mise-archive-keyring.gpg
-Architectures: amd64
-" | sudo tee /etc/apt/sources.list.d/mise.sources
+  dpkg --list | grep --quiet "ii[[:space:]]+mise"
+  if [[ "$?" -eq 1 ]]; then
 
-  sudo apt-get update
-  ensure_installed mise
+    sudo apt-get update --assume-yes
+    sudo apt-get upgrade --assume-yes
+    ensure_installed gpg sudo wget curl
+    sudo install -dm 755 /etc/apt/keyrings
+    wget -qO - "https://mise.jdx.dev/gpg-key.pub" | gpg --dearmor | sudo tee /etc/apt/keyrings/mise-archive-keyring.gpg
 
-fi
+    printf "X-Repolib-Name: mise
+  Enabled: yes
+  Types: deb
+  URIs: https://mise.jdx.dev/deb
+  Suites: stable
+  Components: main
+  Signed-By: /etc/apt/keyrings/mise-archive-keyring.gpg
+  Architectures: amd64
+  " | sudo tee /etc/apt/sources.list.d/mise.sources
 
-mise use --global "chezmoi"
+    sudo apt-get update
+    ensure_installed mise
+
+  fi
+
+}
+
+
+function install_chezmoi() {
+
+  mise use --global --yes "chezmoi"
+
+}
+
+
+install_mise
+install_chezmoi
