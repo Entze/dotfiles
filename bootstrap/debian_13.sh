a12262a181d6617c71537da236da467bea8b59df
Lukas Grassauer <lukas@grassauer.eu>
2024-10-24 16:55:58 +0200

    Add debian_13 bootstrap script

---
+++ bootstrap/debian_13.sh
@@ -0,0 +1,38 @@
+#!/bin/usr/env bash
+
+function ensure_installed() {
+
+  for pkg in "$@"; do
+    listed="$(dpkg --list | grep --quiet "ii[[:space:]]+${pkg}")"
+    if [[ "$listed" -eq 1 ]]; then
+      sudo apt-get install --assume-yes "$pkg"
+    fi
+  done
+
+}
+
+listed="$(dpkg --list | grep --quiet "ii[[:space:]]+mise")"
+if [[ "$listed" -eq 1 ]]; then
+
+  sudo apt-get update --assume-yes
+  sudo apt-get upgrade --assume-yes
+  ensure_installed gpg sudo wget curl
+  sudo install -dm 755 /etc/apt/keyrings
+  wget -qO - "https://mise.jdx.dev/gpg-key.pub" | gpg --dearmor | sudo tee /etc/apt/keyrings/mise-archive-keyring.gpg
+
+  printf "X-Repolib-Name: mise
+Enabled: yes
+Types: deb
+URIs: https://mise.jdx.dev/deb
+Suites: stable
+Components: main
+Signed-By: /etc/apt/keyrings/mise-archive-keyring.gpg
+Architectures: amd64
+" | sudo tee /etc/apt/sources.list.d/mise.sources
+
+  sudo apt-get update
+  ensure_installed mise
+
+fi
+
+mise use --global "chezmoi"
